8d89572a264c9abf8246f967350f2591b8c14495
emil <emiltony007@gmail.com>
2025-03-11 14:44:49 +0530

    js code added

---
+++ maxtwo.js
@@ -0,0 +1,5 @@
+const num1 = prompt("Enter the first number:");
+const num2 = prompt("Enter the second number:");
+
+const largest = (num1 > num2) ? num1 : num2;
+console.log("The largest number is:", largest);
